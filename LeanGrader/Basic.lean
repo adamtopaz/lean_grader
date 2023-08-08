a32cb940ad41dde4c8c71e118b9790a51b790fc0
adamtopaz <github@adamtopaz.com>
2023-08-08 13:41:35 -0600

    cleanup

---
+++ LeanGrader/Basic.lean
@@ -0,0 +1,10 @@
+import Lean
+
+open Lean
+
+elab "#type_hash" id:ident : command => do
+  let id ← resolveGlobalConstNoOverload id
+  let env ← getEnv
+  let some cinfo := env.find? id | throwError s!"{id} not found in environment."
+  IO.println cinfo.type.hash
+
--- Main.lean
+++ Main.lean
@@ -1,26 +1,29 @@
-import «LeanGrader»
+import LeanGrader.Basic
 
-open Lean Elab Command System LeanGrader 
+open Lean Elab Command System  
 
-def main (args : List String) : IO UInt32 := do
-  let some problemFile := args[0]? | throw <| .userError "Usage: grade {problemFile}.jsonl {solutionFile}.lean"
-  let some solutionFile := args[1]? | throw <| .userError "Usage: grade {problemFile}.jsonl {solutionFile}.lean"
-  let lines ← IO.FS.lines ⟨problemFile⟩
-  let .ok probs := lines.mapM Json.parse | throw <| .userError "Failed to parse lines as json."
-  let .ok (probs : Array Problem) := probs.mapM fromJson? | throw <| .userError "Failed to parse lines as problems."
-  let solutionContents ← IO.FS.readFile ⟨solutionFile⟩
+unsafe def main (args : List String) : IO UInt32 := do
+  let some hash := args[0]? | throw <| .userError "Usage: lake exe grade {hash}"
+  let some hash := hash.toNat? | throw <| .userError s!"Failed to parse hash {hash}"
+  let hash : UInt64 := hash.toUInt64
+  let solutionFile : FilePath := "Solution.lean"
+  let solutionContents ← IO.FS.readFile solutionFile
   let inputCtx := Parser.mkInputContext solutionContents "<input>"
-  let (header, _, messages) ← Parser.parseHeader inputCtx
-  --enableInitializersExecution
+  let (header, parserState, messages) ← Parser.parseHeader inputCtx
+  enableInitializersExecution
   initSearchPath (← findSysroot)
-  let (env, _) ← processHeader header {} messages inputCtx
+  let (env, messages) ← processHeader header {} messages inputCtx
+  let cmdState : Command.State := Command.mkState env messages {}
+  let frontEndState ← IO.processCommands inputCtx parserState cmdState
+  let env := frontEndState.commandState.env
   let axioms : Name → (Array Name) := fun nm => (CollectAxioms.collect nm |>.run env |>.run {}).snd.axioms
-  for p in probs do
-    if !env.contains p.name then 
-      IO.println s!"{p.name} not found in environment."
-      return 1
-    if (axioms p.name).size != 0 then 
-      IO.println s!"{p.name} depends on axioms."
-      return 1
-  IO.println "All problems passed."
+  let some solution := env.find? `solution | 
+    IO.println "Solution not found in environment."
+    return 1
+  if solution.type.hash != hash then
+    IO.println "Solution has wrong type."
+    return 1
+  if (axioms solution.name).size != 0 then
+    IO.println "Solution depends on axioms."
+  IO.println "Solution is valid."
   return 0
